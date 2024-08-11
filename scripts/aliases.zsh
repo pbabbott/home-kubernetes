7d46922e9e4620d7dae041c7fc9478acbee92ed9
Brandon Abbott <pbabbott@gmail.com>
2024-08-11 19:12:49 +0000

    Initial setup of devcontainer

---
+++ scripts/aliases.zsh
@@ -0,0 +1,18 @@
+alias chimaera="ssh root@192.168.4.192"
+alias controller="ssh firebolt@192.168.4.193"
+alias worker1="ssh firebolt@192.168.4.194"
+alias worker2="ssh firebolt@192.168.4.195"
+alias worker3="ssh firebolt@192.168.5.81"
+alias dumbledore="ssh albus@192.168.4.157"
+alias ror="ssh pabbott@192.168.4.124"
+alias bpi="ssh pi@192.168.4.144"
+
+alias k="kubectl"
+
+alias fgit="flux reconcile source git flux-system"
+alias fks="flux reconcile ks"
+alias fksfs="flux reconcile ks flux-system"
+alias fksic="flux reconcile ks infra-config"
+alias fksictr="flux reconcile ks infra-controllers"
+alias fksm="flux reconcile ks monitoring"
+alias fall="fgit; fksfs; fksictr; fksic; fksm; fks apps;"
---
+++ scripts/dev-env-setup.sh
@@ -0,0 +1,5 @@
+echo "Creating project aliases"
+cp scripts/aliases.zsh ~/.oh-my-zsh/custom/home-kubernetes-aliases.zsh
+
+echo "Setting up starship"
+cp scripts/starship.toml ~/.config/starship.toml
