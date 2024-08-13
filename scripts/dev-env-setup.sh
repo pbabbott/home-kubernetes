107b468949958b921edaad0880cb60a7ad5ad610
Brandon Abbott <pbabbott@gmail.com>
2024-08-12 22:28:43 -0500

    Add note on directory check to dev-env-setup

--- scripts/dev-env-setup.sh
+++ scripts/dev-env-setup.sh
@@ -10,6 +10,7 @@ echo "Setting up starship"
 cp scripts/files/starship.toml ~/.config/starship.toml
 
 echo "Fetching oh-my-zsh plugins"
+# TODO: wrap each one of these with a directory check
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
 git clone https://github.com/superbrothers/zsh-kubectl-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-kubectl-prompt
